f79632eddc8474de12e77625ff69e4342f4aa985
BonoDV <84352335+BonoDV@users.noreply.github.com>
2025-05-04 13:25:18 +0200

    made 4 more katas

---
+++ Difficult 8/incorrect-division-method.js
@@ -0,0 +1,7 @@
+/* This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+https://www.codewars.com/kata/54d1c59aba326343c80000e7/train/javascript
+
+*/
+
+const solve = (x, y) => x / y;
